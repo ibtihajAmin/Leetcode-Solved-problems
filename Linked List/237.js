28d17777dd6e60a1ee3afbee3c0f9c979e8530f6
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-04-27 21:56:00 +0600

    237 no problem statement is added

---
+++ Linked List/237.js
@@ -0,0 +1,6 @@
+/*Problem Statement : Delete Node in a Linked List
+  Problem Link : https://leetcode.com/problems/delete-node-in-a-linked-list/
+*/
+
+//Driver Function
+const deleteNode = (node) => {};
